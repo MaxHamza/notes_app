5cbf59a556590853d2aa91639028712c80692ec4
unknown <hamza1mks1@gmail.com>
2023-08-04 17:38:21 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const myApp(
+  ));
+}
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DashBoard(),
+    );
+  }
+}
+

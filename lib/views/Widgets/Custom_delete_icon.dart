86586310e90e0469bf7a4fdcec21e434b097b07b
Hamza <hamza1mks1@gmail.com>
2023-08-13 13:34:56 +0300

    CustomNotesApp build

---
+++ lib/views/Widgets/Custom_delete_icon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class DeleteIcon extends StatelessWidget {
+  const DeleteIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(Icons.delete,color: Colors.black,size: 40,);
+  }
+}
---
+++ lib/views/Widgets/custom_notes_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'Costum_Card.dart';
+class CustomListView extends StatelessWidget {
+  const CustomListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Expanded(//we cannot put expanded in stack widget because it is not flexible but row and column is flexible
+      child: ListView.builder(itemBuilder:(context,i)=>const Padding(
+        padding:  EdgeInsets.symmetric(vertical: 4.0),
+        child:  CustomCard(),
+      ),
+        itemCount:10,
+        physics:const BouncingScrollPhysics().parent,
+      ),
+    );
+  }
+}

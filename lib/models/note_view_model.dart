bf697add5a32629d9e4a1081fc868e62d52e9342
Hamza <hamza1mks1@gmail.com>
2023-08-17 00:39:26 +0300

    add note view model

---
+++ lib/models/note_view_model.dart
@@ -0,0 +1,7 @@
+class NoteView {
+  final String title;
+  final String subtitle;
+  final String date;
+  final int color;
+ NoteView({required this.title,required this.subtitle,required this.date,required this.color});
+}
